9eda05d72501d8c4c9e7a57881a4f460d1a21623
Badru2 <hilalbadru4@gmail.com>
2024-02-17 13:08:02 +0700

    Fix can't input text

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     @section('title', 'Create')
-    <div class="p-4 text-white lg:w-3/5 mx-auto">
+    <div class="p-4 mx-auto text-white lg:w-3/5">
         <div class="lg:border-2 md:border-gray-800 md:p-3 md:bg-gray-800">
-            <form action="{{ route('tweet.store') }}" method="POST" enctype="multipart/form-data" class="lg:px-4 py-6">
+            <form action="{{ route('tweet.store') }}" method="POST" enctype="multipart/form-data" class="py-6 lg:px-4">
                 @csrf
                 <div class="mb-4">
                     <h2 class="mb-2">GAMBAR / VIDEO</h2>
@@ -11,37 +11,37 @@
                         name="file" id="selectImage">
 
                     @error('image')
-                        <div class="alert alert-danger mt-2">
+                        <div class="mt-2 alert alert-danger">
                             {{ $message }}
                         </div>
                     @enderror
 
                     <div class="mt-2">
                         <video id="videoPreview" src="#" alt="your video" style="display: none" controls></video>
-                        <img id="imagePreview" src="#" alt="your image" class="mt-3 mx-auto"
+                        <img id="imagePreview" src="#" alt="your image" class="mx-auto mt-3"
                             style="display:none;" />
-                        <audio id="audioPreview" src="#" class="mt-3 mx-auto" style="display: none"
+                        <audio id="audioPreview" src="#" class="mx-auto mt-3" style="display: none"
                             controls></audio>
                     </div>
                 </div>
 
                 <div>
                     <h2 class="mb-4">CONTENT</h2>
-                    <textarea name="content" id="" cols="30" rows="10" class="bg-slate-700 w-full rounded-lg captions"
+                    <textarea name="content" id="" cols="30" rows="10" class="w-full rounded-lg bg-slate-700 captions"
                         id="editor"></textarea>
                 </div>
 
-                <div class="flex w-full justify-evenly mt-5">
+                <div class="flex w-full mt-5 justify-evenly">
                     <a href="{{ route('tweet.create') }}"
-                        class="bg-red-700 px-8 py-1 lg:px-12 lg:py-3 rounded-full cursor-pointer hover:bg-red-500">Reset</a>
+                        class="px-8 py-1 bg-red-700 rounded-full cursor-pointer lg:px-12 lg:py-3 hover:bg-red-500">Reset</a>
                     <input type="submit" value="Publish"
-                        class="bg-cyan-500 px-8 py-1 rounded-full cursor-pointer hover:bg-cyan-300">
+                        class="px-8 py-1 rounded-full cursor-pointer bg-cyan-500 hover:bg-cyan-300">
                 </div>
             </form>
         </div>
     </div>
 
-    <script src="https://cdn.tiny.cloud/1/ug8k3u0xghcson8wedsmu7tihjbd1mg86xrhsdo9eg662qo9/tinymce/6/tinymce.min.js"
+    {{-- <script src="https://cdn.tiny.cloud/1/ug8k3u0xghcson8wedsmu7tihjbd1mg86xrhsdo9eg662qo9/tinymce/6/tinymce.min.js"
         referrerpolicy="origin"></script>
 
     <script>
@@ -51,6 +51,6 @@
             skin: "oxide-dark",
             content_css: "dark"
         });
-    </script>
+    </script> --}}
 
 </x-app-layout>
